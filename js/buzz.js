d53f14880a52b92d188d698841ae2bcbc3b37a3d
laurentbatailley <laurent.batailley@gmail.com>
2016-05-03 23:14:45 +0200

    merged messsage merge

--- js/buzz.js
+++ js/buzz.js
@@ -2,8 +2,5 @@ export  function buzz() {
     console.log('new changes');
 };
 export  function bouzz() {
-    console.log('new bouzz another kikoool changes');
-};
-export  function gorabouzz() {
-    console.log('new bouzz another garabouzz changes');
+    console.log('new bouzz another bouzz changes');
 };
